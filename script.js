9581fa18fd61fc37ffe8dd750760d529fcfe19ff
Tandy <tandy.bon@gmail.com>
2025-03-19 02:46:48 +0200

    page improvements 2

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 let currentQuestion = 1;
 
 function startSurvey() {
-    document.getElementById("welcomeMessage").classList.add("hidden");
+    document.getElementById("welcomeMessage").classList.add("hidden1");
     document.getElementById("survey").classList.remove("hidden");
-
-    document.getElementById(`question${currentQuestion}`).classList.remove("hidden");
+    document.getElementById("question1").classList.remove("hidden");
 }
 
 function nextQuestion(questionNumber) {
     document.getElementById(`question${currentQuestion}`).classList.add("hidden");
 
+    // Set the next question
     currentQuestion = questionNumber + 1;
 
+    // Show the next question or move to game if done
     if (currentQuestion <= 4) {
         document.getElementById(`question${currentQuestion}`).classList.remove("hidden");
     } else {
+        // Hide the survey and show the game
         document.getElementById("survey").classList.add("hidden");
-        document.getElementById("game").classList.remove("hidden");
+        document.getElementById("prizePage").classList.remove("hidden");
     }
-}
-
-function finishGame() {
-    document.getElementById("doneButton").style.display = "block"; // Show the "I'm done" button
 }
 
 function claimPrize() {
